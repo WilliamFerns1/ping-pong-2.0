aea76b05dd29b38ba3150aa9baff480971f64e18
WilliamFerns1 <willfv2@gmail.com>
2024-05-08 21:25:00 +0200

    feat(navbar): made navbar desktop responsive

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ const navItems = document.getElementById("nav-list");
 let isMenuActive = false;
 
 let rotation = 0;
-
-/* Path to get this working: */
-/* 1. Make a keyframe that starts at right 0%, and end at right 5vh at 100%. */
-/* 2. Add keyframe to a class called slide-from-right. The keyframe must take take a ease-in-out of 3s; */
-/* 3. When it is time to show the menu, put slide-from-right class on the nav-items div, then put the display to flex.  */
-/*   Else if it is time to hide the nav items, put on another class that is called "reverse-animation". */
-/*   The "reverse-animation" class will have the following: "animation-direction: reverse;" */
-
 const toggleNavList = () => {
   isMenuActive = !isMenuActive;
   if (isMenuActive) {
