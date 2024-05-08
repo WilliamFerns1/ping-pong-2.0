9c1e459787bf9f7f6b242f5189d27e4b49837102
WilliamFerns1 <willfv2@gmail.com>
2024-05-08 19:57:56 +0200

    feat(menu): added spinning animation to menu button on navbar

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ const navItems = document.getElementById("nav-list");
 
 let isMenuActive = false;
 
-function toggleNavList() {
+let rotation = 0;
+
+const toggleNavList = () => {
   isMenuActive = !isMenuActive;
   if (isMenuActive) {
     navItems.style.display = "flex";
     menuButton.classList = "fa-solid fa-xmark";
+    rotation += 360;
   } else {
     navItems.style.display = "none";
     menuButton.classList = "fa-solid fa-bars";
+    rotation -= 360;
   }
+  menuButton.style.rotate = `${rotation}deg`;
 }
 
 menuButton.addEventListener("click", toggleNavList);
