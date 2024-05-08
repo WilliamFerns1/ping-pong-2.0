83e6866dd6598599483aebc406135341d40be1fd
WilliamFerns1 <willfv2@gmail.com>
2024-05-08 22:04:46 +0200

    feat(navlinks): clean underline animation for nav text

--- script.js
+++ script.js
@@ -6,17 +6,29 @@ let isMenuActive = false;
 let rotation = 0;
 const toggleNavList = () => {
   isMenuActive = !isMenuActive;
-  if (isMenuActive) {
-    navItems.classList = "slide-from-right";
-    navItems.style.display = "flex";
-    menuButton.classList = "fa-solid fa-xmark";
-    rotation += 360;
-  } else {
-    navItems.classList = "slide-from-left";
-    menuButton.classList = "fa-solid fa-bars";
-    rotation -= 360;
-  }
+  toggleMenu(isMenuActive);
   menuButton.style.rotate = `${rotation}deg`;
 }
 
+const isMobile = () => {
+  return window.innerWidth <= 512;
+}
+
+const toggleMenu = (isMenuActive) => {
+  if (isMobile()) {
+    if (isMenuActive) {
+      navItems.classList = "slide-from-right";
+      navItems.style.display = "flex";
+      menuButton.classList = "fa-solid fa-xmark";
+      rotation += 360;
+    } else {
+      navItems.classList = "slide-from-left";
+      menuButton.classList = "fa-solid fa-bars";
+      rotation -= 360;
+    }
+  }
+}
+
 menuButton.addEventListener("click", toggleNavList);
+
+
