7d4a9c5e3fe08ab723e79d257b29f90b0ed54452
WilliamFerns1 <willfv2@gmail.com>
2024-05-15 09:46:06 +0200

    feat(renderElements): render 2 paddles, and ball
    Made two functions: 1 - set canvas size to full witdth and height. Made renderElements function to render three main objects: player1Paddle, player2Paddle, and ball

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-const menuButton = document.getElementById("menu-btn");
-const navItems = document.getElementById("nav-list");
+const gameCanvas = document.getElementById("game-canvas");
 
-let isMenuActive = false;
-
-let rotation = 0;
-const toggleNavList = () => {
-  isMenuActive = !isMenuActive;
-  toggleMenu(isMenuActive);
-  menuButton.style.rotate = `${rotation}deg`;
+const setCanvasSize = () => {
+  gameCanvas.setAttribute("width", window.innerWidth);
+  gameCanvas.setAttribute("height", window.innerHeight);
 }
 
-const isMobile = () => {
-  return window.innerWidth <= 512;
+
+const renderElements = () => {
+  const ball = gameCanvas.getContext("2d");
+  ball.fillStyle= "white";
+  ball.beginPath();
+  ball.arc(95, 50, 20, 0, 2 * Math.PI);
+  ball.strokeStyle = "white";
+  ball.stroke();
+  ball.fill();
+
+  const player1Paddle = gameCanvas.getContext("2d");
+  player1Paddle.fillStyle = "blue";
+  player1Paddle.fillRect(0, 0, 30, 400);
+
+  const player2Paddle = gameCanvas.getContext("2d");
+  player2Paddle.fillStyle = "red";
+  player2Paddle.fillRect(100, 0, 30, 400);
 }
 
-const toggleMenu = (isMenuActive) => {
-  if (isMobile()) {
-    if (isMenuActive) {
-      navItems.classList = "slide-from-right";
-      navItems.style.display = "flex";
-      menuButton.classList = "fa-solid fa-xmark";
-      rotation += 360;
-    } else {
-      navItems.classList = "slide-from-left";
-      menuButton.classList = "fa-solid fa-bars";
-      rotation -= 360;
-    }
-  }
-}
-
-menuButton.addEventListener("click", toggleNavList);
-
-
+renderElements();
